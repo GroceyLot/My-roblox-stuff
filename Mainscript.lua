3e92b848d72b442ce7844192d8626243cd2a7f32
GroceyLot <108685271+GroceyLot@users.noreply.github.com>
2023-04-18 18:34:49 +0100

    Update Mainscript.lua

--- Mainscript.lua
+++ Mainscript.lua
@@ -1,3 +1,46 @@
+-- // Only allow to be executed once
+assert(not getgenv().AntiKickExecuted, "Anti Kick is already running!")
+
+-- // Services
+local Players = game:GetService("Players")
+
+-- // Vars
+local LocalPlayer = Players.LocalPlayer
+local KickAttemptFormat = "Kick attempted for: %s"
+local BlockedMethods = {"Kick", "kick"}
+
+-- // What to do when Kick detected
+local function kickDetected(Player, Reason)
+    warn(KickAttemptFormat:format(Reason))
+end
+
+-- // Index hook
+hookfunction(LocalPlayer.Kick, kickDetected)
+
+-- // Namecall hook
+local __namecall
+__namecall = hookmetamethod(game, "__namecall", function(self, ...)
+    -- // Vars
+    local args = {...}
+    local method = getnamecallmethod()
+    
+    -- // Check if kick attempt
+    if (self == LocalPlayer and table.find(BlockedMethods, method)) then
+        -- // Alert
+        kickDetected(self, args[1])
+        
+        -- // Cancel it
+        return
+    end
+    
+    -- // Return - so everything else that uses namecall does not break
+    return __namecall(self, ...)
+end)
+
+-- // Credits
+getgenv().AntiKickExecuted = true
+messagebox("Loaded.\n\n\n-", "Anti Kick", 0)
+
 -- Define Library
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/UI-Libraries/main/Vynixius/Source.lua"))()
 
