ede9767ffd15196d94bca541fa53f61ee89b4d2d
GroceyLot <108685271+GroceyLot@users.noreply.github.com>
2023-07-01 17:32:06 +0100

    Update and rename Garten of Banban GUI to Garten of Banban GUI.lua

---
+++ Garten of Banban GUI.lua
@@ -0,0 +1,43 @@
+
+
+-- Define Library
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/UI-Libraries/main/Vynixius/Source.lua"))()
+local speed = 10
+-- Create Window
+local Window = Library:AddWindow({
+	title = {"Grocey", "Barry's Prison"},
+	theme = {
+		Accent = Color3.fromRGB(0, 255, 0)
+	},
+	key = Enum.KeyCode.RightShift,
+	default = true
+})
+
+local Features = Window:AddTab("Features", {default = true})
+local Settings = Window:AddTab("Settings", {default = false})
+
+local settingssec = Settings:AddSection("Main", {default = false})
+
+-- Create Picker
+local Picker = settingssec:AddPicker("Accent Colour", {color = Color3.fromRGB(85, 235, 186)}, function(color)
+	Window:SetAccent(color)
+end)
+
+local Bind = settingssec:AddBind("Menu Bind", Enum.KeyCode.RightShift, {default = false, flag = "Bind_Flag"}, function(keycode)
+	Window:SetKey(keycode)
+end)
+
+local Button18 = settingssec:AddButton("Unload", function()
+	game.CoreGui["Vynixius UI Library"]:Destroy()
+end)
+
+local a = Features:AddSection("Game", {default = false})
+local a1 = a:AddSlider("Speed", 10, 20, 10, {toggleable = false, default = false, flag = "Slider_Flag",  fireondrag = true, rounded = true}, function(val)
+	game.Player.LocalPlayer.PlayerGui.MainGui.Stamina:Destroy()
+  speed = val
+end)
+
+while true do
+wait()
+  game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = speed
+end
