16f4086191cca226173029c6d3b1e3953ccadf42
GroceyLot <108685271+GroceyLot@users.noreply.github.com>
2023-07-03 17:53:56 +0100

    Create Glib.lua

---
+++ Glib.lua
@@ -0,0 +1,64 @@
+local Glib = {}
+function Glib.init(BackColor3, FrontColor3, TextColor3, name, parent)
+	local scrgui = Instance.new("ScreenGui")
+	scrgui.Name = "Glib" .. tostring(math.random(0, 999999))
+	scrgui.Parent = parent
+	local objs = {Instance.new("Frame"), Instance.new("UICorner"), Instance.new("TextLabel"), Instance.new("UICorner"), Instance.new("ScrollingFrame")}
+	local cobj = objs[1]
+	cobj.Position = UDim2.new(0.7,0,0.5,0)
+	cobj.Size = UDim2.new(0.275,0,0.465,0)
+	cobj.BackgroundColor3 = BackColor3
+	cobj.Parent = scrgui
+	cobj.Name = "Container"
+	cobj = objs[2]
+	cobj.Parent = objs[1]
+	cobj = objs[3]
+	cobj.Name = "Label"
+	cobj.Text = name
+	cobj.Parent = objs[1]
+	cobj.BackgroundColor3 = FrontColor3
+	cobj.TextColor3 = TextColor3
+	cobj.Position = UDim2.new(0.05,0,0.05,0)
+	cobj.Size = UDim2.new(0.9,0,0.125,0)
+	cobj.TextScaled = true
+	cobj = objs[4]
+	cobj.Parent = objs[3]
+	cobj = objs[5]
+	cobj.Name = "toggles"
+	cobj.Position = UDim2.new(0.05,0,0.2,0)
+	cobj.Size = UDim2.new(0.9,0,0.75,0)
+	cobj.BackgroundTransparency = 1
+	cobj.CanvasSize = UDim2.new(0,0,0,0)
+	cobj.AutomaticCanvasSize = Enum.AutomaticSize.XY
+	cobj.Parent = objs[1]
+	return cobj
+end
+function Glib.AddToggle(window, text, name)
+	local toggle = Instance.new("TextButton")
+	toggle.Name = name
+	toggle.Position = UDim2.new(0,0,0,#window:GetChildren()*55)
+	toggle.Parent = window
+	toggle.Text = text
+	toggle.Size = UDim2.new(1,0,0,50)
+	toggle.TextScaled = true
+	local uic = Instance.new("UICorner")
+	uic.Parent = toggle
+	local bool = Instance.new("BoolValue")
+	bool.Name = "Checked"
+	bool.Parent = toggle
+	toggle.BackgroundColor3 = window.Parent.Label.BackgroundColor3
+	toggle.TextColor3 = window.Parent.Label.TextColor3
+	toggle.MouseButton1Click:Connect(function()
+		bool.Value = not bool.Value
+		if bool.Value then
+			toggle.TextColor = BrickColor.Green()
+		else
+			toggle.TextColor3 = window.Parent.Label.TextColor3
+		end
+	end)
+	return toggle
+end
+function Glib.ConnectToggle(window, togglename, func)
+	local value = window:FindFirstChild(togglename).Checked
+	window:FindFirstChild(togglename).MouseButton1Click:Connect(func(value))
+end
