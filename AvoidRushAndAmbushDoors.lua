c9b95a9debe40abd1e6834ec6e86363604576081
GroceyLot <108685271+GroceyLot@users.noreply.github.com>
2023-07-25 12:29:02 +0100

    Create AvoidRushAndAmbushDoors.lua

---
+++ AvoidRushAndAmbushDoors.lua
@@ -0,0 +1,54 @@
+local teleportEnabled = false -- Set this value to true to enable teleportation
+local isBelowMap = false -- Keep track of the player's teleportation state
+
+local function teleportPlayer(player, isBelow, originalPosition)
+    local character = player.Character
+    local rootPart = character and character:FindFirstChild("HumanoidRootPart")
+
+    if rootPart then
+        if isBelow then
+            -- Teleport the player 30 studs below their original position
+            character:SetPrimaryPartCFrame(CFrame.new(originalPosition - Vector3.new(0, 25, 0)))
+            isBelowMap = true
+        else
+            -- Restore the player to their original position
+            character:SetPrimaryPartCFrame(CFrame.new(originalPosition))
+            isBelowMap = false
+        end
+    end
+end
+
+-- Function to define the original position when RushMoving spawns
+local function onRushMovingAdded(child)
+    if child.Name == "RushMoving" or child.Name == "AmbushMoving" then
+        local player = game.Players.LocalPlayer
+        local character = player.Character
+        local rootPart = character and character:FindFirstChild("HumanoidRootPart")
+        if rootPart then
+            local originalPosition = rootPart.Position
+			for _, part in ipairs(character:GetDescendants()) do
+        		if part:IsA("BasePart") then
+            		part.Anchored = true
+        		end
+    		end
+            while child.Parent == game.Workspace do
+            	wait(0.1)
+            	teleportPlayer(player, true, originalPosition)
+            end
+            teleportPlayer(player, false, originalPosition)
+            for _, part in ipairs(character:GetDescendants()) do
+        		if part:IsA("BasePart") then
+            		part.Anchored = false
+        		end
+    		end
+        end
+    end
+end
+
+-- Connect the function to be called whenever a new child is added to workspace
+game:GetService("Workspace").ChildAdded:Connect(onRushMovingAdded)
+
+-- Set the teleportEnabled value to false to enable restoring the player's position
+-- You can change this value based on game events or player input as needed.
+teleportEnabled = true
+
