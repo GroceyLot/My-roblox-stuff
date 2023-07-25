dc0baf601fb6ddb3d64600c9e185a739c2362aae
GroceyLot <108685271+GroceyLot@users.noreply.github.com>
2023-07-25 10:07:30 +0100

    Update GroceyUiLib.lua

--- GroceyUiLib.lua
+++ GroceyUiLib.lua
@@ -134,9 +134,9 @@ Arrow_2.Size = UDim2.new(0.0511826649, 0, 0.950000107, 0)
 Arrow_2.Image = "http://www.roblox.com/asset/?id=278544479"
 Arrow_2.MouseButton1Click:Connect(function()
       if BoxContainer.Size == UDim2.new(1, 0, 13, 0) then
-        Utils:TweenSize(BoxContainer, TweenInfo.new(0.5, Enum.EasingStyle.Sine, Enum.EasingDirection.InOut), UDim2.new(1, 0, 13, 0), UDim2.new(1, 0, 1, 0))
+        Utils:TweenSize(BoxContainer, TweenInfo.new(0.5, Enum.EasingStyle.Sine, Enum.EasingDirection.InOut), UDim2.new(1, 0, 13, 0), UDim2.new(1, 0, 0.01, 0))
       else
-        Utils:TweenSize(BoxContainer, TweenInfo.new(0.5, Enum.EasingStyle.Sine, Enum.EasingDirection.InOut), UDim2.new(1, 0, 1, 0), UDim2.new(1, 0, 13, 0))
+        Utils:TweenSize(BoxContainer, TweenInfo.new(0.5, Enum.EasingStyle.Sine, Enum.EasingDirection.InOut), UDim2.new(1, 0, 0.01, 0), UDim2.new(1, 0, 13, 0))
       end
 end)
 
