759ce2fe9c883eead287c793866ded73f869c6a9
GroceyLot <108685271+GroceyLot@users.noreply.github.com>
2023-07-26 17:30:18 +0100

    Update Doors.lua

--- Doors.lua
+++ Doors.lua
@@ -75,7 +75,6 @@ local function onRushMovingAdded(child)
                 local rushMoving = workspace:FindFirstChild("RushMoving")
 		wait(0.1)
 		character:MoveTo(closet.Main.Position)
-		print(done)
                 if rushMoving:FindFirstChild("RushNew") and (rushMoving:FindFirstChild("RushNew").Position - rootPart.Position).Magnitude < 200 and closet.HiddenPlayer.Value == false then
                 	fireproximityprompt(closet.HidePrompt)
 		else
