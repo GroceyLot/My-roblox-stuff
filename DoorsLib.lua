8740e9db4272c11b5332e36a7c87a74a04237356
GroceyLot <108685271+GroceyLot@users.noreply.github.com>
2023-07-31 08:48:50 +0100

    Update DoorsLib.lua

--- DoorsLib.lua
+++ DoorsLib.lua
@@ -309,7 +309,7 @@ end)
 			return Button
 		end
 		function SectionTable:FinishSize()
-			Frame.Size = Udim2.new(0.96, 0, 0.05, ScrollingFrame_2.AbsoluteCanvasSize.Y)
+			Frame.Size = UDim2.new(0.96, 0, 0.05, ScrollingFrame_2.AbsoluteCanvasSize.Y)
 		end
 		return SectionTable
 	end
