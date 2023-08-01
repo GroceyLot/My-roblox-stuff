31da2e94ec3dd6c1bbf7dbd75d9f38bf297fc95f
GroceyLot <108685271+GroceyLot@users.noreply.github.com>
2023-08-01 17:39:07 +0100

    Update GLoader.lua

--- GLoader.lua
+++ GLoader.lua
@@ -1,7 +1,8 @@
 local placeid = game.PlaceId
-local list = {1242235469 = "https://raw.githubusercontent.com/GroceyLot/My-roblox-stuff/Things/Ball%20and%20Axe%20GUI.lua", 6839171747 = "https://github.com/GroceyLot/My-roblox-stuff/raw/Things/GSDoors.lua", 189707 = "https://github.com/GroceyLot/My-roblox-stuff/raw/Things/Natural%20Disaster%20Survival%20GUI.lua", 8712817601 = "https://raw.githubusercontent.com/GroceyLot/My-roblox-stuff/Things/Barry's%20Prison%20Run%20GUI"}
-for i, v in pairs(list) do
-  if tonumber(i) == placeid then
-    loadstring(game:HttpGet(v))()
+local ids = {1242235469, 6839171747, 189707, 8712817601}
+local list = {"https://raw.githubusercontent.com/GroceyLot/My-roblox-stuff/Things/Ball%20and%20Axe%20GUI.lua", "https://github.com/GroceyLot/My-roblox-stuff/raw/Things/GSDoors.lua", "https://github.com/GroceyLot/My-roblox-stuff/raw/Things/Natural%20Disaster%20Survival%20GUI.lua", "https://raw.githubusercontent.com/GroceyLot/My-roblox-stuff/Things/Barry's%20Prison%20Run%20GUI"}
+for i = 1, # ids do
+  if ids[i] == placeid then
+    loadstring(game:HttpGet(list[i]))()
   end
 end
