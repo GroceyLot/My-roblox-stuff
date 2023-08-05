2d9cd79dfdaa912cd4341b3fd9fce0076e0f9b55
GroceyLot <108685271+GroceyLot@users.noreply.github.com>
2023-08-05 13:27:35 +0100

    Update GSX.lua

--- GSX.lua
+++ GSX.lua
@@ -13,25 +13,30 @@ function Lib:InitConnections()
 	Lib.Connection = game.TextChatService.TextChannels.RBXGeneral.MessageReceived:Connect(function(ms)
 		if ms.Text == "[GSX Internal] Recieved connection request, adding to playerlist" and ms.TextSource.Name ~= game.Players.LocalPlayer.Name then
 			table.insert(Lib.Plrs, ms.TextSource)
+			wait()
 			game.TextChatService.TextChannels.RBXGeneral:SendAsync("[GSX Internal] Connection added")
 		end
 		if ms.Text == "[GSX Internal] ServerSide executor ran, waiting for connections" and ms.TextSource.Name ~= game.Players.LocalPlayer.Name then
 			game.TextChatService.TextChannels.RBXGeneral:SendAsync("[GSX Internal] Recieved connection request, adding to playerlists")
+			wait()
 			table.insert(Lib.Plrs, ms.TextSource)
 			game.TextChatService.TextChannels.RBXGeneral:SendAsync("[GSX Internal] Connection added")
 		end
 		if ms.Text == "[GSX Internal] Request to remove self from playerlist" and ms.TextSource.Name ~= game.Players.LocalPlayer.Name then
 			game.TextChatService.TextChannels.RBXGeneral:SendAsync("[GSX Internal] Recieved removal request, removing")
+			wait()
 			table.remove(Lib.Plrs, ms.TextSource)
 			game.TextChatService.TextChannels.RBXGeneral:SendAsync("[GSX Internal] Connection removed")
 		end
 		for i=1, #Lib.Plrs do
 			if Lib.Plrs[i] == ms.TextSource and ms.MetaData ~= nil and ms.Text == "[GSX Internal] Script send request" then
 				game.TextChatService.TextChannels.RBXGeneral:SendAsync("[GSX Internal] Recieved script, running", ms.MetaData)
+				wait()
 				loadstring(ms.MetaData)
 			end
 			if Lib.Plrs[i] == ms.TextSource and ms.MetaData ~= nil and ms.Text == "[GSX Internal] Script send request for: "..game.Players.LocalPlayer.Name then
 				game.TextChatService.TextChannels.RBXGeneral:SendAsync("[GSX Internal] Recieved script for client, running", ms.MetaData)
+				wait()
 				loadstring(ms.MetaData)
 			end
 			if Lib.Plrs[i] == ms.TextSource and ms.MetaData ~= nil and ms.Text == "[GSX Internal] Recieved script, running" then
