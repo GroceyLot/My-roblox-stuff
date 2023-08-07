7b9a3ba2a69e289928a4963ffea7b5499ea90383
GroceyLot <108685271+GroceyLot@users.noreply.github.com>
2023-08-07 16:53:06 +0100

    Update GLoader.lua

--- GLoader.lua
+++ GLoader.lua
@@ -1,6 +1,6 @@
 local placeid = game.PlaceId
-local ids = {1242235469, 6839171747, 189707, 8712817601}
-local list = {"https://raw.githubusercontent.com/GroceyLot/My-roblox-stuff/Things/Ball%20and%20Axe%20GUI.lua", "https://github.com/GroceyLot/My-roblox-stuff/raw/Things/GSDoors.lua", "https://github.com/GroceyLot/My-roblox-stuff/raw/Things/Natural%20Disaster%20Survival%20GUI.lua", "https://raw.githubusercontent.com/GroceyLot/My-roblox-stuff/Things/Barry's%20Prison%20Run%20GUI"}
+local ids = {1242235469, 6839171747, 8712817601}
+local list = {"https://raw.githubusercontent.com/GroceyLot/My-roblox-stuff/Things/Ball%20and%20Axe%20GUI.lua", "https://github.com/GroceyLot/My-roblox-stuff/raw/Things/GSDoors.lua",  "https://raw.githubusercontent.com/GroceyLot/My-roblox-stuff/Things/Barry's%20Prison%20Run%20GUI"}
 for i = 1, # ids do
   if ids[i] == placeid then
     loadstring(game:HttpGet(list[i]))()
