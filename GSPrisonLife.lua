6fa8347816d44769846a16bb3756a9a812cfb12d
GroceyLot <108685271+GroceyLot@users.noreply.github.com>
2023-08-11 07:44:02 +0100

    Update GSPrisonLife.lua

--- GSPrisonLife.lua
+++ GSPrisonLife.lua
@@ -4,4 +4,32 @@ local Uitils = loadstring(game:HttpGet("https://raw.githubusercontent.com/Regula
 local Achievements = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Custom%20Achievements/Source.lua"))()
 local Lib = loadstring(game:HttpGet("https://github.com/GroceyLot/My-roblox-stuff/raw/Things/DoorsLib.lua"))()
 
-Lib:Window(
+local Window = Lib:Window("PL", "Grocey - Prison Life")
+
+local Player = Window:Section("PLR", "Player")
+local Combat = Window:Section("CBT", "Combat")
+local World = Window:Section("WOD", "World")
+
+
+World:Toggle("Remove Fences", "RF", false, function(val)
+    pcall(function()
+        if val then 
+            game:GetService("Workspace")["Prison_Fences"].Parent = game.ReplicatedStorage
+        else
+            game.ReplicatedStorage["Prison_Fences"].Parent = game.Workspace
+        end
+    end)
+end)
+World:Toggle("Remove Doors", "RD", false, function(val)
+    pcall(function()
+        if val then 
+            game:GetService("Workspace")["Doors"].Parent = game.ReplicatedStorage
+        else
+            game.ReplicatedStorage["Doors"].Parent = game.Workspace
+        end
+    end)
+end)
+Player:FinishSize()
+Combat:FinishSize()
+World:FinishSize()
+
