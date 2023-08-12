68f28e0fc5a3258e0e7a3b630db4b4a3ba453807
GroceyLot <108685271+GroceyLot@users.noreply.github.com>
2023-08-12 07:25:44 +0100

    Update GSPrisonLife.lua

--- GSPrisonLife.lua
+++ GSPrisonLife.lua
@@ -1,4 +1,5 @@
 
+
 local Utils = loadstring(game:HttpGet("https://raw.githubusercontent.com/GroceyLot/My-roblox-stuff/Things/Utilla.lua"))()
 local Uitils = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/UI.lua"))()
 local Lib = loadstring(game:HttpGet("https://github.com/GroceyLot/My-roblox-stuff/raw/Things/DoorsLib.lua"))()
@@ -6,15 +7,16 @@ local Lib = loadstring(game:HttpGet("https://github.com/GroceyLot/My-roblox-stuf
 local Window = Lib:Window("PL", "Grocey - Prison Life")
 
 local Player = Window:Section("Player", "PLR")
+local Visuals = Window:Section("Visuals", "VSL")
 local Combat = Window:Section("Combat", "CBT")
 local World = Window:Section("World", "WOD")
-
+local Respawn
 local flySpeed = 25
 local flying = false
 
 local function UpdateFlying()
     if flying then
-		game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = true
+        game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = true
         local camera = game.Workspace.CurrentCamera
         local moveDirection = Vector3.new(0, 0, 0)
 
@@ -23,76 +25,76 @@ local function UpdateFlying()
         if input:IsKeyDown(Enum.KeyCode.W) then
             moveDirection = moveDirection + camera.CFrame.LookVector
         end
-        
+
         if input:IsKeyDown(Enum.KeyCode.S) then
             moveDirection = moveDirection - camera.CFrame.LookVector
         end
-        
+
         if input:IsKeyDown(Enum.KeyCode.A) then
             moveDirection = moveDirection - camera.CFrame.RightVector
         end
-        
+
         if input:IsKeyDown(Enum.KeyCode.D) then
             moveDirection = moveDirection + camera.CFrame.RightVector
         end
-        
+
         if input:IsKeyDown(Enum.KeyCode.E) then
             moveDirection = moveDirection + Vector3.new(0, 1, 0)
         end
-        
+
         if input:IsKeyDown(Enum.KeyCode.Q) then
             moveDirection = moveDirection - Vector3.new(0, 1, 0)
         end
 
-		game.Players.LocalPlayer.Character:TranslateBy(moveDirection * (flySpeed/25))
+        game.Players.LocalPlayer.Character:TranslateBy(moveDirection * (flySpeed/25))
     end
 end
 
 
 Player:Toggle("Fly+Noclip", "FY", false, function(val)
     flying = val
-	game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = val
+    game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = val
 end)
 
 Player:Number("Flyspeed", "FS", 0, 300, 25, function(val)
-	flySpeed = val
+    flySpeed = val
 end)
 local Noclip = nil
 local Clip = nil
 
 function noclip()
-	Clip = false
-	local function Nocl()
-		if Clip == false and game.Players.LocalPlayer.Character ~= nil then
-			for _,v in pairs(game.Players.LocalPlayer.Character:GetDescendants()) do
-				if v:IsA('BasePart') and v.CanCollide and v.Name ~= floatName then
-					v.CanCollide = false
-				end
-			end
-		end
-		wait(0.21) -- basic optimization
-	end
-	Noclip = game:GetService('RunService').Stepped:Connect(Nocl)
+    Clip = false
+    local function Nocl()
+        if Clip == false and game.Players.LocalPlayer.Character ~= nil then
+            for _,v in pairs(game.Players.LocalPlayer.Character:GetDescendants()) do
+                if v:IsA('BasePart') and v.CanCollide and v.Name ~= floatName then
+                    v.CanCollide = false
+                end
+            end
+        end
+        wait(0.21) -- basic optimization
+    end
+    Noclip = game:GetService('RunService').Stepped:Connect(Nocl)
 end
 
 function clip()
-	if Noclip then Noclip:Disconnect() end
-	Clip = true
+    if Noclip then Noclip:Disconnect() end
+    Clip = true
 end
 
 Player:Toggle("Noclip", "N", false, function(val)
     if val then
-		noclip()
-	else
-		clip()
-	end
+        noclip()
+    else
+        clip()
+    end
 end)
 
-Player:Number("Fov", "FV", 0, 120, math.round(game.Workspace.CurrentCamera.FieldOfView), function(val)
-	game.Workspace.CurrentCamera.FieldOfView = val
+Visuals:Number("Fov", "FV", 0, 120, math.round(game.Workspace.CurrentCamera.FieldOfView), function(val)
+    game.Workspace.CurrentCamera.FieldOfView = val
 end)
 Player:Number("Walkspeed", "WS", 0, 1000, game.Players.LocalPlayer.Character.Humanoid.WalkSpeed, function(val)
-	game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = val
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = val
 end)
 
 -- Connect to changes in the val variable to update flying mode
@@ -105,35 +107,35 @@ Combat:Toggle("Loop Kill All", "LK", false, function(val)
 end)
 
 function ting()
-   if killing then
-      local plrs = game.Players:GetChildren()
-      for i=1,#plrs do
-         pcall(function()
-         local char = game.Players.LocalPlayer.Character
-         if plrs[i].Character:FindFirstChild("HumanoidRootPart") then
-            if plrs[i] ~= game.Players.LocalPlayer then
-               for j=1,30 do
-                  char:MoveTo(plrs[i].Character.HumanoidRootPart.Position)
-                  game.ReplicatedStorage.meleeEvent:FireServer(plrs[i])
-                  wait(0.0001)
-               end
+    if killing then
+        local plrs = game.Players:GetChildren()
+        for i=1,#plrs do
+            pcall(function()
+                local char = game.Players.LocalPlayer.Character
+                if plrs[i].Character:FindFirstChild("HumanoidRootPart") then
+                    if plrs[i] ~= game.Players.LocalPlayer then
+                        for j=1,30 do
+                            char:MoveTo(plrs[i].Character.HumanoidRootPart.Position)
+                            game.ReplicatedStorage.meleeEvent:FireServer(plrs[i])
+                            wait(0.0001)
+                        end
+                    end
+                    wait(0.1)
+                end
+            end)
+            if not killing then
+                game.RunService.Heartbeat:Once(ting)
+                return
             end
-            wait(0.1)
-         end
-         end)
-         if not killing then
-            game.RunService.Heartbeat:Once(ting)
-            return
-         end
-      end
-   end
-   game.RunService.Heartbeat:Once(ting)
+        end
+    end
+    game.RunService.Heartbeat:Once(ting)
 end
 game.RunService.Heartbeat:Once(ting)
 
 World:Toggle("Remove Fences", "RF", false, function(val)
     pcall(function()
-        if val then 
+        if val then
             game:GetService("Workspace")["Prison_Fences"].Parent = game.ReplicatedStorage
         else
             game.ReplicatedStorage["Prison_Fences"].Parent = game.Workspace
@@ -142,14 +144,50 @@ World:Toggle("Remove Fences", "RF", false, function(val)
 end)
 World:Toggle("Remove Doors", "RD", false, function(val)
     pcall(function()
-        if val then 
+        if val then
             game:GetService("Workspace")["Doors"].Parent = game.ReplicatedStorage
         else
             game.ReplicatedStorage["Doors"].Parent = game.Workspace
         end
     end)
 end)
+World:Toggle("Respawn at grave", "RG", false, function(val)
+    if val then
+		local function death()
+			local player = game.Players.LocalPlayer
+            local character = player.Character
+
+            if character then
+                print("die")
+                local humanoidRootPart = character:WaitForChild("HumanoidRootPart")
+                local respawnPosition = humanoidRootPart.Position
+
+                local characterAddedConnection
+                characterAddedConnection = player.CharacterAdded:Connect(function(newCharacter)
+                    newCharacter:WaitForChild("HumanoidRootPart")
+
+                    -- Delay a short time to ensure that the new character has loaded properly
+                    wait(0.1)
+                    for i=1,100 do
+                        newCharacter:SetPrimaryPartCFrame(CFrame.new(respawnPosition))
+                        wait(0.01)
+                    end
+                    print("done")
+                    characterAddedConnection:Disconnect()
+                end)
+				Respawn = game.Players.LocalPlayer.Character.Humanoid.Died:Once(death)
+				print("Added connection")
+            end
+		end
+        Respawn = game.Players.LocalPlayer.Character.Humanoid.Died:Once(death)
+		print("Added connection")
+    else
+        Respawn:Disconnect()
+		print("Removed connection")
+    end
+end)
 
 Player:FinishSize()
 Combat:FinishSize()
 World:FinishSize()
+Visuals:FinishSize()
