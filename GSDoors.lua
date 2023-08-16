b256cc0dce9b8b3b3b5d2fc803cb3ade407c271c
GroceyLot <108685271+GroceyLot@users.noreply.github.com>
2023-08-16 08:24:23 +0100

    Update GSDoors.lua

--- GSDoors.lua
+++ GSDoors.lua
@@ -75,15 +75,40 @@ local arct = section2:Toggle("Auto Rush Closet", "arc", false, function(state)
     		vs["arc"] = state
 	end
 end)
+local Noclip = nil
+local Clip = nil
+
+function noclip()
+    Clip = false
+    local function Nocl()
+        if Clip == false and game.Players.LocalPlayer.Character ~= nil then
+            for _,v in pairs(game.Players.LocalPlayer.Character:GetDescendants()) do
+                if v:IsA('BasePart') and v.CanCollide and v.Name ~= floatName then
+                    v.CanCollide = false
+                end
+            end
+        end
+        wait(0.21) -- basic optimization
+    end
+    Noclip = game:GetService('RunService').Stepped:Connect(Nocl)
+end
+
+function clip()
+    if Noclip then Noclip:Disconnect() end
+    Clip = true
+end
+
 local gmnb = section2:Toggle("Godmode/Noclip bypass", "gmn", false, function(state)
 	if not vs["arc"] then
     		vs["gmn"] = state
 		if vs["gmn"] then
 			local Col = game.Players.LocalPlayer.Character:FindFirstChild("Collision")
 			Col.Position = Col.Position - Vector3.new(0,10,0)
+			noclip()
 		else
 			local Col = game.Players.LocalPlayer.Character:FindFirstChild("Collision")
 			Col.Position = Col.Position + Vector3.new(0,10,0)
+			clip()
 		end
 	end
 end)
