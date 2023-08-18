eba5b3ce2b888d2a369bc281e921e58b33fb482a
GroceyLot <108685271+GroceyLot@users.noreply.github.com>
2023-08-18 16:16:33 +0100

    Update GSX.lua

--- GSX.lua
+++ GSX.lua
@@ -32,15 +32,13 @@ function Lib:InitConnections()
 		for i=1, #Lib.Plrs do
 			if Lib.Plrs[i] == ms.TextSource and ms.TextSource.Name ~= game.Players.LocalPlayer.Name and ms.Metadata ~= nil and ms.Text == "[GSX Internal] Script send request" then
 				print(ms.Metadata)
-				local string = tostring(ms.Metadata)
-				loadstring(string)
+				loadstring(tostring(ms.Metadata))
 				wait(0.1)
 				game.TextChatService.TextChannels.RBXGeneral:SendAsync("[GSX Internal] Recieved script, running")
 			end
 			if Lib.Plrs[i] == ms.TextSource and ms.Metadata ~= nil and ms.Text == "[GSX Internal] Script send request for: "..game.Players.LocalPlayer.Name then
 				print(ms.Metadata)
-				local string = tostring(ms.Metadata)
-				loadstring(string)
+				loadstring(tostring(ms.Metadata))
 				wait(0.1)
 				game.TextChatService.TextChannels.RBXGeneral:SendAsync("[GSX Internal] Recieved script for client, running")
 			end
