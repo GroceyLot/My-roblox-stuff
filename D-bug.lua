68f9f8fbdf6cb8f21a2b7f9277598e904ff3ebad
GroceyLot <108685271+GroceyLot@users.noreply.github.com>
2024-03-18 09:42:37 +0000

    Update D-bug.lua

--- D-bug.lua
+++ D-bug.lua
@@ -1,9 +1,22 @@
 local Log = {}
 local InstProp = loadstring(game:HttpGet("https://github.com/GroceyLot/My-roblox-stuff/raw/Things/InstProp.lua"))()
+_G.Logging = true
 
 function UpdateLog()
   for i,v in pairs(game:GetDescendants()) do
     local Object = {Name = v:GetFullName(), Properties = {}}
+    for k,w in pairs(InstProp:Get(v.ClassName)) do
+      Properties[W] = tostring(v[W])
+    end  
     table.insert(Log, Object)
   end
 end
+
+while _G.Logging do
+  UpdateLog
+  task.wait(0.5)
+end
+
+local JsonLog = game.HttpService:JSONEncode(Log)
+
+writefile("Log.json", JsonLog)
